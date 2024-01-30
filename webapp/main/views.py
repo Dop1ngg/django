d5edffb133cefe25e53be9e1e39ede85b9810734
Dop1ngg <135999501+Dop1ngg@users.noreply.github.com>
2024-01-31 02:27:30 +0300

    URLs routing

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,15 +1,24 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': "Home",
-        'content': "Главная страница",
+        'title': "Home - главная",
+        'content': "Магазин мебели HOME",
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About')
+    context = {
+        'title': "Home - О нас",
+        'content': "О нас",
+        'text_on_page': """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Aenean risus metus, pellentesque a lorem et, aliquet accumsan ligula.
+                            Praesent consectetur nibh eros, at dapibus odio fermentum nec. Sed in ultrices elit.
+                            Morbi sem ante, tincidunt non arcu sit amet, fringilla tincidunt urna.
+                            In ante massa, bibendum suscipit lectus at, aliquet rutrum dolor."""
+    }
+
+    return render(request, 'main/about.html', context)
